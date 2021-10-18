2104b32f569d16835f3aed10dc943fe520e20667
Calvin Donner <calvinjdonner@gmail.com>
2021-10-17 22:03:22 -0600

    updated file structure

--- db/schema.sql
+++ db/schema.sql
@@ -16,20 +16,21 @@ CREATE TABLE roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id VARCHAR(30) NOT NULL
-    -- CONSTRAINT `fk_department` 
-    -- FOREIGN KEY (department_id) 
-    -- REFERENCES department(department_name) 
-    -- ON UPDATE CASCADE
+    department_id INTEGER,
+    CONSTRAINT `fk_department` 
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) 
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE employees(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_name INTEGER
-    -- CONSTRAINT fk_roles 
-    -- FOREIGN KEY (role_name) 
-    -- REFERENCES roles(title) 
-    -- ON UPDATE CASCADE
+    role_id INTEGER,
+    manager_id INTEGER UNSIGNED,
+    CONSTRAINT `fk_roles` 
+    FOREIGN KEY (role_id) 
+    REFERENCES roles(id) 
+    ON UPDATE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
-INSERT INTO employees (first_name, last_name, role_name)
-VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 2),
-  ('Piers', 'Gaveston', 3),
-  ('Charles', 'LeRoi', 2),
-  ('Katherine', 'Mansfield', 4),
-  ('Dora', 'Carrington', 6),
-  ('Edward', 'Bellamy', 4),
-  ('Montague', 'Summers', 6),
-  ('Octavia', 'Butler', 7),
-  ('Unica', 'Zurn', 5);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES
-('Sales Lead', 140000, 1),
-('Salesperson', 95000, 1),
-('Lead Engineer', 150000, 2),
-('Staff Enginner', 110000, 2),
-('Legal Team Lead', 300000, 3),
-('Lawyer', 175000, 3),
-('Analyst', 105000, 4);
-
  INSERT INTO department (department_name)
  VALUES
  ('Sales'),
  ('Engineering'),
  ('Legal'),
  ('Supply Chain');
+ 
+INSERT INTO roles (title, salary)
+VALUES
+('Sales Lead', 140000),
+('Salesperson', 95000),
+('Lead Engineer', 150000),
+('Staff Enginner', 110000),
+('Legal Team Lead', 300000),
+('Lawyer', 175000),
+('Analyst', 105000);
+
+INSERT INTO employees (first_name, last_name, manager_id)
+VALUES
+  ('Ronald', 'Firbank',  NULL),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 1),
+  ('Charles', 'LeRoi', NULL),
+  ('Katherine', 'Mansfield', 4),
+  ('Dora', 'Carrington', 4),
+  ('Edward', 'Bellamy', NULL),
+  ('Montague', 'Summers', 7),
+  ('Octavia', 'Butler', 7),
+  ('Unica', 'Zurn', NULL );
