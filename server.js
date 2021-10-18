28692bef9c055eb858e29eac9d05afdfc3626e5a
Calvin Donner <calvinjdonner@gmail.com>
2021-10-17 22:15:28 -0600

    questions set up

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const db = require('db/connection');
+
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+function intro() {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "questions",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee role",
+            "Quit"
+        ]
+    })
+    .then(function(answer) {
+        if (answer.action === 'View all departments') {
+            viewDepartments();
+        } else if (answer.action === 'View all roles') {
+            viewRoles();
+        } else if (answer.action === 'View all employees') {
+            viewEmployees();
+        } else if (answer.action === 'Add a department') {
+            addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update employee role') {
+            updateRole();
+        }
+        else if (answer.action === 'Exit') {
+            connection.end();
+        }
+    })
+}
